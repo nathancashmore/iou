030f9bc1ea9db716f00a3661445a821cffa776e4
NathanCashmore <stat1cv0id>
2017-12-21 15:38:16 +0000

    Update k8 deployment scripts

--- scripts/deploy-docker-image-to-k8s.sh
+++ scripts/deploy-docker-image-to-k8s.sh
@@ -4,5 +4,5 @@ docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker tag $REPO:$COMMIT $REPO:latest;
 docker push $REPO;
 
-kubectl --kubeconfig kube/kubeconfig set image deployment/$APP $APP=$REPO:$COMMIT;
-kubectl --kubeconfig kube/kubeconfig rollout history deployment/$APP;
+kubectl set image deployment/$APP $APP=$REPO:$COMMIT;
+kubectl rollout history deployment/$APP;
